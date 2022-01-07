204a71cd8a9bcaebfc16b60baea9f671d2651113
Udit1 <udgupta155@gmail.com>
2022-01-08 00:22:37 +0530

    Conditional Statements

---
+++ Day 2/nested_if_else.py
@@ -0,0 +1,11 @@
+height = int(input("Enter Height in cm"))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("Enter your age"))
+    if age >= 18:
+        print("Please Pay ₹100")
+    else:
+        print("Please Pay ₹50")
+else:
+    print("Complane Piyo")
