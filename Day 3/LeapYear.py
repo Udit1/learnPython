1438f17df0e3a3aed631dcad1ceac6ee68aac698
Udit1 <udgupta155@gmail.com>
2022-01-08 09:58:01 +0530

    Day 3

---
+++ Day 3/LeapYear.py
@@ -0,0 +1,14 @@
+# In Leap year we have 366 days
+# Every 4 years
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
