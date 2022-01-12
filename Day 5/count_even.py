f01b8d886a134b46e676284b0fb76559e0ba1a90
Udit150 <udgupta155@gmail.com>
2022-01-12 11:17:00 +0530

    Day 5

---
+++ Day 5/count_even.py
@@ -0,0 +1,11 @@
+sum = 0
+for i in range(0, 101, 2):
+    sum += i
+
+print(sum)
+
+
+# Formula to calculate
+# n = 100
+# sum = (n*(n+1))/2
+# print(sum)
---
+++ Day 5/range_with_timer.py
@@ -0,0 +1,5 @@
+import time
+
+for i in range(1, 60):
+    time.sleep(0.5)
+    print(i, end="\b\b\b")
