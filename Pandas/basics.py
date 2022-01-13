d1e6e9beda2e7de459d8db9664fbcf50d3fe58c8
Udit150 <udgupta155@gmail.com>
2022-01-13 10:32:09 +0530

    Day 7

---
+++ Pandas/basics.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+
+df = pd.read_csv('Pandas/annual.csv')
+print(df.to_string())
+
+# Normal Array to Table
+# Length should be the same
+mydataset = {
+    'cars': ["BMW", "Volvo", "Ford", "Maruti"],
+    'passings': [3, 7, 2, 6]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
+
+# check version of pandas
+print(f"Pandas Version: {pd.__version__}")
+
+# Series
+
+name = ["A", "B", "C", "D"]
+a = pd.Series(name)
+print(a)
+
+
+a = [1, 7, 2]
+
+myvar = pd.Series(a, index=["x", "y", "z"])
+
+print(myvar)
---
+++ Pandas/csv_learn.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import os
+
+df = pd.read_csv('Pandas/annual.csv')
+print(df.to_dict())
