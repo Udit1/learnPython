979e7b90174c308303726a00842a0027574c808e
Udit150 <udgupta155@gmail.com>
2022-01-14 08:04:55 +0530

    Commit on Day 8

--- Day 7/hangman_5.py
+++ Day 7/hangman_5.py
@@ -1,97 +1,36 @@
 import random
-import words
-from modules import arts
+from modules import words, arts
 
+end_of_game = False
+chosen_word = random.choice(words.word_list)
+word_length = len(chosen_word)
+lives = 6
 print(f"{arts.logo}")
+print(f'Pssst, the solution is {chosen_word}.', )
+display = []
 
-# stages = ['''
-#   +---+
-#   |   |
-#   O   |
-#  /|\  |
-#  / \  |
-#       |
-# =========
-# ''', '''
-#   +---+
-#   |   |
-#   O   |
-#  /|\  |
-#  /    |
-#       |
-# =========
-# ''', '''
-#   +---+
-#   |   |
-#   O   |
-#  /|\  |
-#       |
-#       |
-# =========
-# ''', '''
-#   +---+
-#   |   |
-#   O   |
-#  /|   |
-#       |
-#       |
-# =========''', '''
-#   +---+
-#   |   |
-#   O   |
-#   |   |
-#       |
-#       |
-# =========
-# ''', '''
-#   +---+
-#   |   |
-#   O   |
-#       |
-#       |
-#       |
-# =========
-# ''', '''
-#   +---+
-#   |   |
-#       |
-#       |
-#       |
-#       |
-# =========
-# ''']
+for _ in range(word_length):
+    display += "_"
 
-# end_of_game = False
-# word_list = ["ardvark", "baboon", "camel"]
-# chosen_word = random.choice(word_list)
-# word_length = len(chosen_word)
-# lives = 6
-# print(f"{logo}")
-# print(f'Pssst, the solution is {chosen_word}.', )
-# display = []
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-# for _ in range(word_length):
-#     display += "_"
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
 
-# while not end_of_game:
-#     guess = input("Guess a letter: ").lower()
+    if guess not in chosen_word:
+        lives -= 1
+        print(lives)
+        print(f"{arts.stages.pop()}")
+        if lives == -1:
+            end_of_game = True
+            print("You Loss!")
+            break
 
-#     for position in range(word_length):
-#         letter = chosen_word[position]
-#         if letter == guess:
-#             display[position] = letter
+    print(f"{' '.join(display)}")
 
-#     if guess not in chosen_word:
-#         lives -= 1
-#         print(lives)
-#         print(f"{stages.pop()}")
-#         if lives == -1:
-#             end_of_game = True
-#             print("You Loss!")
-#             break
-
-#     print(f"{' '.join(display)}")
-
-#     if "_" not in display:
-#         end_of_game = True
-#         print("You win.")
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
