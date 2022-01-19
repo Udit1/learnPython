00456ab3710e8430dd2af177fdff5c0312956db9
Udit150 <udgupta155@gmail.com>
2022-01-19 16:44:06 +0530

    Commit

--- Program Practice/Pattern/HardPattern.py
+++ Program Practice/Pattern/HardPattern.py
@@ -1,8 +1,8 @@
 print("""
 -------------------------------------Pattern 1-------------------------------------""")
-num = 10
+num = 5
 for row in range(1, 5):
-    for column in range(1, row+1):
-        print(num, end=" ")
-        num -= 1
+    for column in range(5, row+1, -1):
+        print(num, end="")
+    num -= 1
     print()
